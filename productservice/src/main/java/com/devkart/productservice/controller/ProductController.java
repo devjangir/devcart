47a7d2bc006b54777446be7a936078267f148492
devjangir <devpassion7@gmail.com>
2024-02-20 21:31:39 +0530

    Product Service and User Service created with sample requests and added docker component

---
+++ productservice/src/main/java/com/devkart/productservice/controller/ProductController.java
@@ -0,0 +1,14 @@
+package com.devkart.productservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/products")
+public class ProductController {
+    @GetMapping
+    public String getProducts() {
+        return "Get all products";
+    }
+}
